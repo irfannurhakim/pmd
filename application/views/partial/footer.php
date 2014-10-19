3b0835c214f577ad1bcd2a22764c6a9bcf95a36f
Irfan Nurhakim <myfunareaa@gmail.com>
2014-10-20 01:06:59 +0700

    init

---
+++ application/views/partial/footer.php
@@ -0,0 +1,24 @@
+            </div>
+        </section>       
+
+        <script src="/pmd/public/js/jquery-1.11.1.min.js"></script>
+        <script src="/pmd/public/js/jquery-migrate-1.2.1.min.js"></script>
+        <script src="/pmd/public/js/bootstrap.min.js"></script>
+        <script src="/pmd/public/js/modernizr.min.js"></script>
+        <script src="/pmd/public/js/pace.min.js"></script>
+        <script src="/pmd/public/js/retina.min.js"></script>
+        <script src="/pmd/public/js/jquery.cookies.js"></script>
+        
+        <script src="/pmd/public/js/flot/jquery.flot.min.js"></script>
+        <script src="/pmd/public/js/flot/jquery.flot.resize.min.js"></script>
+        <script src="/pmd/public/js/flot/jquery.flot.spline.min.js"></script>
+        <script src="/pmd/public/js/jquery.sparkline.min.js"></script>
+        <script src="/pmd/public/js/morris.min.js"></script>
+        <script src="/pmd/public/js/raphael-2.1.0.min.js"></script>
+        <script src="/pmd/public/js/bootstrap-wizard.min.js"></script>
+        <script src="/pmd/public/js/select2.min.js"></script>
+
+        <script src="/pmd/public/js/custom.js"></script>
+        <script src="/pmd/public/js/dashboard.js"></script>
+    </body>
+</html>
