4f3bb2fa4e328f1a8799a524a3624e81bacc6047
arif hidayat <tujuhjendral@gmail.com>
2014-10-29 18:19:46 +0700

    Forgot password module

--- application/controllers/forgot.php
+++ application/controllers/forgot.php
@@ -17,60 +17,116 @@ class Forgot extends CI_Controller {
 	public function init(){
 		$username = $this->input->post('username');
 
-		//cek email ke database
-		$user = $this->builtbyprime->get('TBL_USER', array('username' => $username), TRUE);
-		//$user = $this->builtbyprime->explicit("SELECT * FROM TBL_USER where username = '$username'");
-
+		//cek email 
+		$user = $this->builtbyprime->get('TBL_USER',array('USERNAME'=>$username), TRUE);
 		if(!$user){
-			echo "Username tidak dikenal.";
+			echo "Username tidak dikenal / belum terdaftar.";
 			exit();
 		}
 
-    $factory = new RandomLib\Factory;
-    $generator = $factory->getMediumStrengthGenerator();
-    //Generate Token
-    $token = $generator->generateString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	    $factory   = new RandomLib\Factory;
+	    $generator = $factory->getMediumStrengthGenerator();
+	    
+	    //Generate Token
+	    $token      = $generator->generateString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+		
+		$this->builtbyprime->update('TBL_FORGOT_PASSWORD',array('EMAIL'=>$user['EMAIL']),array('IS_VALID'=>0));
 
-    $this->builtbyprime->insert('TBL_FORGOT_PASSWORD', array('token' => $token, 'email' => $user['EMAIL']));
-    //$this->builtbyprime->explicit("INSERT INTO TBL_FORGOT_PASSWORD (TOKEN, EMAIL) VALUES ('".$token."', '".$user['EMAIL']."')");
+		//insert token
+	    $insert = $this->builtbyprime->insert('TBL_FORGOT_PASSWORD',array('token'=>$token,'EMAIL'=>$user['EMAIL']));
+	  
+		// kirim email
+		$config = Array(
+		        'protocol'  => 'smtp',
+		        'smtp_host' => 'ssl://smtp.googlemail.com',
+		        'smtp_port' => '465',
+		        'smtp_user' => 'dev@aegis.co.id',
+      			'smtp_pass' => 'aegis10201',
+		        'mailtype'  => 'html',
+		        'starttls'  => true,
+		        'newline'   => "\r\n"
+	      );
 
+	    $this->load->library('email', $config);
+	    $this->email->from('noreply@Aegis.co.id', 'Forgot Password Confirmation');
+	    $this->email->to($user['EMAIL']);
+	    $this->email->subject('Forgot Password Confirmation');
 
+	    $name = $user["NAME"];
+	    $url  = ''.base_url().'forgot/confirm/?email='.$user['EMAIL'].'&token='.$token.'';
+	    $mailContent = <<< EOT
+		   Hi, $name <br />
+		   Anda mengirimkan permintaan untuk me-reset passsword anda.<br /><br />
+		   Silahkan klik link berikut ini untuk me-reset password :<br /> $url
+EOT;
+ 
+   		$this->email->message($mailContent);
 
-		// if(!$user['email']){
-		// 	echo "Data email anda tidak ditemukan, Hubungi Administrator.";
-		// 	exit();
-		// } 
-
-		//TODO kirim email
-		// $this->load->library('email');
-
-		// $this->email->from('myfunareaa@gmail.com', 'Admin PM Dashboard');
-		// $this->email->to('irfannurhaim@s.itb.ac.id'); 
-		// // $this->email->cc('another@another-example.com'); 
-		// // $this->email->bcc('them@their-example.com'); 
-
-		// $this->email->subject('Email Test');
-		// $this->email->message('Testing the email class.');	
-
-		// $this->email->send();
-
-  //   //debug
-  //   echo $this->email->print_debugger();
-		//set response
-		echo "Cek email anda untuk proses pemulihan password anda.";
+	    if($this->email->send())
+	    {
+			echo "Cek email anda untuk proses pemulihan password.";
+	    }
+	    else
+	    {
+  			echo $this->email->print_debugger();
+	    }
 	}
 
-	public function process(){
+	public function confirm(){
 		$email = $this->input->get('email');
 		$token = $this->input->get('token');
+   		
+   		$data = $this->builtbyprime->get('TBL_FORGOT_PASSWORD',array('TOKEN'=>$token,'EMAIL'=> $email,'IS_VALID'=>1),TRUE);
 
-		//TODO reset password kemudian kirim ke email
-    $data = $this->builtbyprime->get('TBL_FORGOT_PASSWORD', array('TOKEN' => $token, 'EMAIL' => $email), TRUE);
+	    if(!$data){
+	       show_404();
+	    }
 
-    if(!$data){
-      show_404();
-    }
+	    $password = rand(111111,999999);
+	    $this->builtbyprime->update('TBL_USER',array('EMAIL'=>$data['EMAIL']),array('PASSWORD'=>md5($password)));
 
-		$this->load->view('authentication');
+		// kirim email
+		$config = Array(
+		        'protocol'  => 'smtp',
+		        'smtp_host' => 'ssl://smtp.googlemail.com',
+		        'smtp_port' => '465',
+		        'smtp_user' => 'dev@aegis.co.id',
+      			'smtp_pass' => 'aegis10201',
+		        'mailtype'  => 'html',
+		        'starttls'  => true,
+		        'newline'   => "\r\n"
+	      );
+
+	    $this->load->library('email', $config);
+	    $this->email->from('noreply@Aegis.co.id', 'New Password');
+	    $this->email->to($data['EMAIL']);
+	    $this->email->subject('New Password');
+
+	    $mailContent = <<< EOT
+   			<br /><br />
+   			Password Baru Anda : $password
+EOT;
+ 
+   		$this->email->message($mailContent);
+
+   		if($this->email->send())
+	    {
+			$this->load->view('forgot/confirm');
+	    }
+	    else
+	    {
+  			echo $this->email->print_debugger();
+	    }
+	}
+
+	public function change_pass(){
+		$data['email'] = '';
+		if($this->input->get('email') != '')
+		{
+			$data['email'] = $this->input->get('email');
+		}
+
+	    if(!$data){ show_404();}
+		$this->load->view('forgot/change_password',$data);
 	} 
 }
