98635b38ea2781f2333ea7a9489f6dbe65fc31ae
arif hidayat <tujuhjendral@gmail.com>
2014-10-31 08:53:10 +0700

    item task

--- application/controllers/item_task.php
+++ application/controllers/item_task.php
@@ -10,12 +10,13 @@ class Item_task extends CI_Controller {
     $data['project'] = $this->builtbyprime->get('TBL_PROJECT');
     $data['item']    = $this->builtbyprime->get('TBL_ITEM_TASK');
 
-    $data['item_list']    = $this->builtbyprime->get('TBL_ITEM_TASK');
+    $data['item_list']    = $this->builtbyprime->explicit('SELECT A.*,B.NAME PARENT FROM TBL_ITEM_TASK A LEFT JOIN TBL_ITEM_TASK B ON A.ID_PARENT = B.ID ORDER BY A.ID_PARENT');
 		$this->load->view('item_task/index', $data);
 	}
 
 	public function add(){
-    $idItem = $this->builtbyprime->explicit("SELECT nvl(max(ID),0) + 1 max FROM TBL_ITEM_TASK");
+    $is_edit = $this->input->post('is-edit');
+    $id      = $this->input->post('id');
 
     $data = array(
       'a' => $this->input->post('id-project'),
@@ -24,27 +25,57 @@ class Item_task extends CI_Controller {
       'd' => $this->input->post('value'),
       'e' => $this->input->post('volume'),
       'f' => $this->input->post('satuan'),
-      'g' => $idItem[0]['MAX'],
       'h' => $this->session->userdata('USERNAME')
     );
 
-    $res = $this->builtbyprime->explicit("INSERT INTO TBL_ITEM_TASK
-                                        (id, id_parent, id_project, name, value, volume, satuan, created_date, modified_date, created_by, modified_by) 
-                                        VALUES 
-                                        ('".$data['g']."','".$data['b']."','".$data['a']."','".$data['c']."','".$data['d']."','".$data['e']."','".$data['f']."',SYSDATE, SYSDATE, '".$data['h']."','".$data['h']."')");
+    if($is_edit == 1){ //edit
+      $this->builtbyprime->update('TBL_ITEM_TASK',array('ID'=>$id),
+                                    array(
+                                            'id_parent'=>$data['b'],
+                                            'id_project'=>$data['a'],
+                                            'name'=>$data['c'],
+                                            'value'=>$data['d'],
+                                            'volume'=>$data['e'],
+                                            'satuan'=>$data['f'],
+                                            //'modified_date'=>date('d/m/Y H:i:s'),
+                                            'modified_by'=>$data['h'],
+                                          )
+                                  );
+    }
+    else
+    {
+      $idItem = $this->builtbyprime->explicit("SELECT nvl(max(ID),0) + 1 max FROM TBL_ITEM_TASK");
+      $res    = $this->builtbyprime->explicit("INSERT INTO TBL_ITEM_TASK
+                                          (id, id_parent, id_project, name, value, volume, satuan, created_date, modified_date, created_by, modified_by) 
+                                          VALUES 
+                                          ('".$idItem[0]['MAX']."','".$data['b']."','".$data['a']."','".$data['c']."','".$data['d']."','".$data['e']."','".$data['f']."',SYSDATE, SYSDATE, '".$data['h']."','".$data['h']."')");
+    }
     
     echo json_encode($res);
 	}
+
+  public function id($id){
+    $user = $this->builtbyprime->get('TBL_ITEM_TASK',array('ID'=>$id), TRUE);
+
+    if($user){
+      echo json_encode(array('status' => 'ok', 'data' => $user));
+    } else {
+      echo json_encode(array('status' => 'error'));
+    }
+  }
 
 	public function view(){
     $this->load->view('project/detail');
 	}
 
-	public function remove(){
+	public function remove($id){
+    $remove = $this->builtbyprime->delete('TBL_ITEM_TASK',array('ID'=>$id));
 
-	}
-
-	public function edit(){
+    if($remove){
+      echo json_encode(array('status' => 'ok'));
+    } else {
+      echo json_encode(array('status' => 'error'));
+    }
 
 	}
 }
--- application/views/item_task/index.php
+++ application/views/item_task/index.php
@@ -1,22 +1,33 @@
 <div>
   <button class="btn btn-primary add-data" data-toggle="modal" data-target=".modal-add-item"><i class="fa fa-plus"></i> Buat Item</button>
 </div>
-<br />
-<table class="table" id="list-item">
-  <th>
-    <td>Nama</td>
-    <td>Value</td>
-    <td>Volume</td>
-    <td>Satuan</td>
-  </th>
-<?php foreach($item_list as $val): ?>
-  <tr>
-    <td><?=$val['NAME'];?></td>
-    <td><?=$val['VALUE'];?></td>
-    <td><?=$val['VOLUME'];?></td>
-    <td><?=$val['SATUAN'];?></td>
-  </tr>
-<?php endforeach; ?>
+<table class="table table-bordered responsive table-hover" id="table-list-item">
+  <thead class="">
+    <tr>
+      <th>Nama Item</th>
+      <th>Parent Item</th>
+      <th>Value</th>
+      <th>Volume</th>
+      <th>Satuan</th>
+      <th width="60px"></th>
+    </tr>
+  </thead>
+
+  <tbody class="selectable">
+    <?php foreach($item_list as $val): ?>
+      <tr>
+        <td><?=$val['NAME'];?></td>
+        <td><?=$val['PARENT'];?></td>
+        <td><?=$val['VALUE'];?></td>
+        <td><?=$val['VOLUME'];?></td>
+        <td><?=$val['SATUAN'];?></td>
+        <td class="dt-cols-center">
+          <a data-toggle="tooltip" title="Edit" class="tooltips edit-row" object="<?php echo $val['ID'];?> "><i class="fa fa-pencil"></i></a> &nbsp;&nbsp;
+          <a data-toggle="tooltip" title="Hapus" class="tooltips delete-row" object="<?php echo $val['ID'];?>"><i class="fa fa-trash-o"></i></a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
 </table>
 <div class="modal fade modal-add-item" tabindex="-1" role="dialog" id="modal-add-item" >
   <div class="modal-dialog">
@@ -42,8 +53,9 @@
                   <div class="form-group">
                       <label class="col-sm-4 control-label">Project<span class="asterisk">*</span></label>
                       <div class="col-sm-8">
-                          <select id="select-id-project" data-placeholder="Pilih Project" class="width300" name="id-project">
-                            <?php foreach ($project as $row) { ?>
+                          <select id="select-id-project" data-placeholder="Pilih Project" class="width300" name="id-project" title="Project wajib dipilih" required>
+                             <option></option>
+                             <?php foreach ($project as $row) { ?>
                                 <option value="<?php echo $row['ID'];?>"><?php echo $row['NAME'];?></option>
                             <?php }; ?>                                          
                           </select>
@@ -52,8 +64,9 @@
                   <div class="form-group">
                       <label class="col-sm-4 control-label">Parent Item</label>
                       <div class="col-sm-8">
-                          <select id="select-id-parent" data-placeholder="Pilih Project" class="width300" name="id-parent">
-                            <?php foreach ($parent as $row) { ?>
+                          <select id="select-id-parent" data-placeholder="Pilih Parent Item" class="width300" name="id-parent">
+                             <option></option>
+                             <?php foreach ($item as $row) { ?>
                                 <option value="<?php echo $row['ID'];?>"><?php echo $row['NAME'];?></option>
                             <?php }; ?>                                          
                           </select>
@@ -64,31 +77,31 @@
                   <div class="form-group">
                       <label class="col-sm-4 control-label">Nama Item<span class="asterisk">*</span></label>
                       <div class="col-sm-8">
-                          <input type="text" name="name" class="form-control" required title="Kolom Nama Item wajib diisi!" />
+                          <input type="text" name="name" id="name" class="form-control" required title="Kolom Nama Item wajib diisi!" />
                       </div>
                   </div>
                   <div class="form-group">
                       <label class="col-sm-4 control-label">Value<span class="asterisk">*</span></label>
                       <div class="col-sm-5">
-                          <input type="text" name="value" class="form-control" required title="Kolom Value wajib diisi!" value="0" />
+                          <input type="text" name="value" id="value" class="form-control" required title="Kolom Value wajib diisi!" value="0" />
                       </div>
                   </div>
                    <div class="form-group">
                       <label class="col-sm-4 control-label">Volume<span class="asterisk">*</span></label>
                       <div class="col-sm-5">
-                          <input type="text" name="volume" class="form-control" required title="Kolom Volume wajib diisi!" value="0" />
+                          <input type="text" name="volume" id="volume" class="form-control" required title="Kolom Volume wajib diisi!" value="0" />
                       </div>
                   </div>
                    <div class="form-group">
                       <label class="col-sm-4 control-label">Satuan<span class="asterisk">*</span></label>
                       <div class="col-sm-5">
-                          <input type="text" name="satuan" class="form-control" required title="Kolom Satuan wajib diisi!"  />
+                          <input type="text" name="satuan" id="satuan" class="form-control" required title="Kolom Satuan wajib diisi!"  />
                       </div>
                   </div>
 
                   <!-- Hidden Field -->
-                  <input type="hidden" name="id" value="-1" />
-                  <input type="hidden" name="is-edit" value="0" />
+                  <input type="hidden" name="id" id="id" value="-1" />
+                  <input type="hidden" name="is-edit" id="is-edit" value="0" />
                 </div><!-- panel-body -->
                 <div class="panel-footer">
                     <button class="btn btn-primary mr5" type="submit">Submit</button>
@@ -105,14 +118,70 @@
 
 <script type="text/javascript">
   $(document).ready(function(){
-    jQuery('#select-id-project, #select-id-parent').select2();
 
+    jQuery('#table-list-item').DataTable({
+      "responsive": true
+    });
+
+    jQuery('#select-id-project,#select-id-parent').select2();
+
+    $('.add-data').click(function(){
+      $('#form-add-item').trigger('reset');
+      $('#select-id-project,#select-id-parent').select2('data', null);
+    });
     // Submit add item
     $('#form-add-item').ajaxForm({
       success: function(a,b,c,d){
         $('#modal-add-item').modal('hide');
         $('.modal-backdrop').hide();
         items();
+
+        //reset status
+        $("#is-edit").val('0');
+      }
+    });
+
+    $('.edit-row').click(function(){
+      var id = $(this).attr('object');
+      //$("select #select-id-parent option[value='"+id+"']").attr('disabled',true);
+
+      $.ajax({
+        url: '<?php echo base_url();?>item_task/id/' + id,
+        dataType: 'json'
+      })
+      .done(function(response, textStatus, jqhr){
+        if(response.status == "ok"){
+          $('#select-id-project').val(response.data.ID_PROJECT).trigger('change');
+          $('#select-id-parent').val(response.data.ID_PARENT).trigger('change');
+          $("#name").val(response.data.NAME);
+          $("#value").val(response.data.VALUE);
+          $("#volume").val(response.data.VOLUME);
+          $("#satuan").val(response.data.SATUAN);
+          $("#id").val(response.data.ID);
+          $("#is-edit").val('1');
+        }
+      })
+      .fail(function(){
+
+      });
+
+      $('.modal-add-item').modal('show');
+    });
+
+    $('.delete-row').click(function(){
+      var id = $(this).attr('object');
+      
+      if (confirm('Apakah anda yakin menghapus item ini ?')) { 
+        $.ajax({
+          url: '<?php echo base_url();?>item_task/remove/' + id,
+          dataType: 'json'
+        })
+        .done(function(response, textStatus, jqhr){
+          items();
+        })
+        .fail(function(){
+
+        });
       }
     });
 
